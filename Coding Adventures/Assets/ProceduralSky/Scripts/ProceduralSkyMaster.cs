edf49eecea43f1148e1b50890722917d0793c3f4
SlenderFox <36467674+SlenderFox@users.noreply.github.com>
2022-01-12 12:27:44 +1100

    Started a procedural sky project

---
+++ Coding Adventures/Assets/ProceduralSky/Scripts/ProceduralSkyMaster.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralSkyMaster : MonoBehaviour
+{
+    [SerializeField]
+    private ComputeShader m_csSkyShader = null;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
